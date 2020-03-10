f8c60365104ce013330890d2d7d76e13c02ab8ce
y4q1b <joel.vbroek@gmail.com>
2020-03-10 15:39:42 -0700

    added gui initialisation code

--- src/SatDB.java
+++ src/SatDB.java
@@ -1,3 +1,29 @@
+import javax.swing.*;
+
 public class SatDB {
+
+    public static void main(String[] args) {
+
+        initialiseDatabase();
+        createAndShowGUI();
+
+
+    }
+
+    private static void initialiseDatabase() {
+        //
+    }
+    private static void createAndShowGUI() {
+        //
+        JFrame f = new JFrame("A JFrame");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(250, 250);
+        f.setLocation(300,200);
+        SatUI newContentPane = new SatUI();
+        f.setContentPane(newContentPane);
+        newContentPane.setOpaque(true);
+        f.pack();
+        f.setVisible(true);
+    }
 
 }
